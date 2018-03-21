cecda03ddd16ee0f01a773d32bb6e61766e5728f
Rushil Perera <pererarushil1940@gmail.com>
2018-03-21 14:57:38 -0400

    Added new fields to models

--- src/main/kotlin/ingester/models/Ingredient.kt
+++ src/main/kotlin/ingester/models/Ingredient.kt
@@ -2,4 +2,9 @@ package ingester.models
 
 import com.google.gson.annotations.SerializedName
 
-data class Ingredient(@SerializedName("ingredientID") val id: Int, @SerializedName("displayValue") val name: String)
+data class Ingredient(
+    @SerializedName("ingredientID") val id: Int,
+    @SerializedName("displayValue") val name: String,
+    val grams: Float,
+    val displayType: String
+)
--- src/main/kotlin/ingester/models/Recipe.kt
+++ src/main/kotlin/ingester/models/Recipe.kt
@@ -1,10 +1,15 @@
 package ingester.models
 
 import com.google.gson.JsonObject
+import com.google.gson.annotations.SerializedName
 
 data class Recipe(
     val ingredients: List<Ingredient>,
     val title: String,
     val nutrition: Map<String, JsonObject>,
-    val servings: Int
+    val servings: Int,
+    val prepMinutes: Int,
+    val cookMinutes: Int,
+    val readyMinutes: Int,
+    @SerializedName("imageUrl") val imageURL: String
 )
