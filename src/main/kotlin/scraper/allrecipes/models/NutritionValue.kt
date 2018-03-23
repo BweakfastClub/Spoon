57690d0ba8e2561abdbd633f896dcde0e41e9b53
Rushil Perera <pererarushil1940@gmail.com>
2018-03-23 11:41:06 -0400

    NutritionValue string fields are now nullable

--- src/main/kotlin/scraper/allrecipes/models/NutritionValue.kt
+++ src/main/kotlin/scraper/allrecipes/models/NutritionValue.kt
@@ -3,10 +3,10 @@ package scraper.allrecipes.models
 import com.google.gson.annotations.SerializedName
 
 data class NutritionValue(
-        val name: String,
-        val amount: Float,
-        val unit: String,
-        val displayValue: String,
-        @SerializedName("percentDailyValue") val dailyValue: String,
-        @SerializedName("hasCompleteData") val isCompleteData: Boolean
+    val name: String?,
+    val amount: Float,
+    val unit: String?,
+    val displayValue: String?,
+    @SerializedName("percentDailyValue") val dailyValue: String?,
+    @SerializedName("hasCompleteData") val isCompleteData: Boolean
 )
