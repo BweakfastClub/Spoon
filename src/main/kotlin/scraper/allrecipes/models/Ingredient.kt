8e3cee0af9a5397b317ce63427217824a52ee168
Rushil Perera <pererarushil1940@gmail.com>
2018-03-23 11:40:46 -0400

    Reformatted models

--- src/main/kotlin/scraper/allrecipes/models/Ingredient.kt
+++ src/main/kotlin/scraper/allrecipes/models/Ingredient.kt
@@ -1,3 +1,8 @@
 package scraper.allrecipes.models
 
-data class Ingredient(val ingredientID: Int, val displayValue: String, val grams: Float, val displayType: String)
+data class Ingredient(
+    val ingredientID: Int,
+    val displayValue: String,
+    val grams: Float,
+    val displayType: String
+)
